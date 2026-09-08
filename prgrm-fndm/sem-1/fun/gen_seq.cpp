37d5e9bb27b3b542564f2136f92b6bf5c361ae15
Bongo <hashir447079@gmail.com>
2026-09-08 18:46:12 +0500

    working fib

--- prgrm-fndm/sem-1/fun/gen_seq.cpp
+++ prgrm-fndm/sem-1/fun/gen_seq.cpp
@@ -5,12 +5,26 @@ int n;
 
 void fib() {
   cout << "Fibonnaci seq is: \n ";
-  int c = 0;
-  int s = 0;
-  
-  while(n <= c)
+  int c = 0; // count
+  int s = 0; // sum
+  int d = 0; // curr digit
+  int p1 = 0; // prev 1st digit
+  int p2 = 0; // prev 2nd digit
+
+  while(c < n)
   {
-    
+    cout << d << ", ";
+
+    if( c == 1 || c == 0)
+    {
+      d = 1;
+    }
+    else {
+      d = p1 + p2;
+    }
+    p2 = p1;
+    p1 = d;
+    s += d;
     c++;
   }
 
