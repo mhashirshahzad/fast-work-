976cf5a299a7a1f615c99ebbe670b0d33899f626
Bongo <hashir447079@gmail.com>
2026-09-09 16:50:04 +0500

    welocme works :3

---
+++ prgrm-fndm/sem-1/op-zero-day/welcome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  cout << "    A      LL       GGGGGG   OOOOOOOO  RRRRRRR  IIIIII TTTTTTTT  HH    HH  MMMM  MMMM " << endl;
+  cout << "   A A     LL       GG       OO    OO  RR   RR    II      TT     HH    HH  MM  MM  MM " << endl;
+  cout << "  AAAAA    LL       GG  GGG  OO    OO  RRRRRRR    II      TT     HHHHHHHH  MM      MM " << endl;
+  cout << " A     A   LL       GG    G  OO    OO  RRRR       II      TT     HH    HH  MM      MM " << endl;
+  cout << "A       A  LLLLLLL  GGGGGGG  OOOOOOOO  RR  RRR  IIIIII    TT     HH    HH  MM      MM " << endl;
+  return 0;
+}
